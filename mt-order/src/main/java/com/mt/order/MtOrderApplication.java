861e090c11c15dab3d8d9ced59266a708b8c62da
wind <liuxianzhie@qq.com>
2021-08-09 10:39:55 +0800

    feat(项目): 新增订单order模块

---
+++ mt-order/src/main/java/com/mt/order/MtOrderApplication.java
@@ -0,0 +1,13 @@
+package com.mt.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtOrderApplication.class, args);
+	}
+
+}
---
+++ mt-order/src/test/java/com/mt/order/MtOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mt.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
