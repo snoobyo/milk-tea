d1aa0b992dd0669f214d73877fda63a6ae2a1283
wind <liuxianzhie@qq.com>
2021-08-09 17:51:50 +0800

    feat(订单服务): 服务注册

--- mt-order/src/main/java/com/mt/order/MtOrderApplication.java
+++ mt-order/src/main/java/com/mt/order/MtOrderApplication.java
@@ -2,7 +2,12 @@ package com.mt.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * @author wind
+ */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MtOrderApplication {
 
